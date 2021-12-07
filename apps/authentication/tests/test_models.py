e34b649cf31e11f8ca9ec5cab8e4a2077421ba9d
Stephen Mwangi <st3v3n.mw@gmail.com>
2021-12-07 20:04:25 +0300

    Change tests to pytest

--- apps/authentication/tests/test_models.py
+++ apps/authentication/tests/test_models.py
@@ -1,58 +1,49 @@
-from django.test import TestCase
-from django.utils.translation import ugettext_lazy as _
+import pytest
 from model_bakery import baker
 
 from apps.authentication.models import User
 
+pytestmark = pytest.mark.django_db
 
-class CustomUserManagerTestModel(TestCase):
-    def setUp(self) -> None:
-        gen_user = baker.make(User, phone_number="+254712345678")
-        self.default_fields = gen_user.__dict__
-        for field in ["email", "password", "_state", "uuid", "is_superuser"]:
-            self.default_fields.pop(field)
-        return super().setUp()
 
-    def test_create_user(self):
-        normal_user = User.objects.create_user(
-            "email@provider.com", "some-password", **self.default_fields
+@pytest.fixture
+def user_default_fields():
+    defaults = baker.make(
+        User, first_name="Jane", last_name="Doe", phone_number="+254712345678"
+    ).__dict__
+    for field in ["email", "password", "_state", "uuid", "is_superuser"]:
+        defaults.pop(field)
+    return defaults
+
+
+@pytest.fixture
+def normal_user(user_default_fields) -> User:
+    return User.objects.create_user(
+        "email@provider.com", "some-password", **user_default_fields
+    )
+
+
+@pytest.fixture
+def admin(user_default_fields) -> User:
+    return User.objects.create_superuser(
+        "email@provider.com", "some-password", **user_default_fields
+    )
+
+
+def test_normal_user(normal_user):
+    assert normal_user.is_staff is False
+    assert str(normal_user) == "Jane Doe"
+    assert normal_user.get_full_name() == "Jane Doe"
+    assert normal_user.get_short_name() == "Jane"
+
+
+def test_admin(admin):
+    assert admin.is_staff is True
+
+
+def test_create_admin_is_superuser_false(user_default_fields):
+    user_default_fields["is_superuser"] = False
+    with pytest.raises(ValueError, match="Superuser must have is_superuser=True."):
+        User.objects.create_superuser(
+            "email@provider.com", "some-password", **user_default_fields
         )
-        self.assertEqual(normal_user.is_staff, False)
-
-    def test_create_superuser(self):
-        default_fields = {**self.default_fields}
-        default_fields["is_superuser"] = False
-        with self.assertRaises(ValueError) as e:
-            User.objects.create_superuser(
-                "email@provider.com", "some-password", **default_fields
-            )
-        self.assertEqual(str(e.exception), _("Superuser must have is_superuser=True."))
-
-        default_fields["is_superuser"] = True
-        admin = User.objects.create_superuser(
-            "email@provider.com", "some-password", **default_fields
-        )
-        self.assertEqual(admin.is_staff, True)
-
-
-class UserTestModel(TestCase):
-    def setUp(self):
-        self.normal_user = baker.make(
-            User,
-            first_name="Jane",
-            last_name="Doe",
-            is_superuser=False,
-            phone_number="+254712345678",
-        )
-        self.admin = baker.make(User, is_superuser=True, phone_number="+254712345678")
-
-    def test_string_representation(self):
-        self.assertEqual(str(self.normal_user), "Jane Doe")
-
-    def test_is_staff(self):
-        self.assertEqual(self.normal_user.is_staff, False)
-        self.assertEqual(self.admin.is_staff, True)
-
-    def test_get_name(self):
-        self.assertEqual(self.normal_user.get_full_name(), "Jane Doe")
-        self.assertEqual(self.normal_user.get_short_name(), "Jane")
--- test.sh
+++ test.sh
@@ -3,6 +3,5 @@
 flake8 apps elixir
 if [ $?  == "0" ];
 then
-    coverage run --source='apps' manage.py test
-    coverage report -m
+    pytest --cov=apps --cov-report=term-missing --cov-branch --cov-fail-under=100 .
 fi
